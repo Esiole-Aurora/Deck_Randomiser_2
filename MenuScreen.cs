e426faebc4b24deb89c398cb3a6579219dc0a94b
Esiole-Aurora <fredfoster0406@gmail.com>
2025-11-27 15:39:05 +0000

    Added documentation to MenuScreen.cs

--- MenuScreen.cs
+++ MenuScreen.cs
@@ -3,29 +3,42 @@ using System.Windows.Forms;
 
 namespace Deck_Randomiser_2;
 
+/// <summary>
+/// MENU SCREEN
+/// Contains buttons that link to the following forms:
+/// - Deck Selector
+/// - Statistics Calculator
+/// </summary>
 public partial class MenuScreen : Form
 {
-    /*
-     * MENU SCREEN
-     * Contains buttons to select and load the following forms:
-     * Deck Selector
-     * Statistical Calculator
-     */
+    /// <summary>
+    /// Constructor for this class
+    /// </summary>
     public MenuScreen()
     {
         InitializeComponent();
         
     }
+    /// <summary>
+    /// Instantiates deck selector form, hides this form
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Deck_Selector_Button_Click_1(object sender, EventArgs e)
     {
-        NumberSelect numberSelect = new NumberSelect();
+        var numberSelect = new NumberSelect();
         numberSelect.Show();
         this.Hide();
     }
     
+    /// <summary>
+    /// Instantiates calculator form, hides this form
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Stats_Button_Click(object sender, EventArgs e)
     {
-        StatsCalc statsCalc = new StatsCalc();
+        var statsCalc = new StatsCalc();
         statsCalc.Show();
         this.Hide();
     }
