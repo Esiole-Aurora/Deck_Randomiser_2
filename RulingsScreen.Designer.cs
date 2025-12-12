a18b8d6ce965a6f8972cbe825a9e00bc279dc488
Esiole-Aurora <fredfoster0406@gmail.com>
2025-12-12 12:03:49 +0000

    Added functionality to get rulings from scryfall API in RulingsScreen.cs

--- RulingsScreen.Designer.cs
+++ RulingsScreen.Designer.cs
@@ -32,6 +32,9 @@ partial class RulingsScreen
     private void InitializeComponent()
     {
         BackButton = new System.Windows.Forms.Button();
+        CardName = new System.Windows.Forms.TextBox();
+        EnterButton = new System.Windows.Forms.Button();
+        Rulings = new System.Windows.Forms.RichTextBox();
         SuspendLayout();
         // 
         // BackButton
@@ -45,16 +48,54 @@ partial class RulingsScreen
         BackButton.UseVisualStyleBackColor = true;
         BackButton.Click += BackButton_Click;
         // 
+        // CardName
+        // 
+        CardName.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        CardName.Location = new System.Drawing.Point(15, 15);
+        CardName.Name = "CardName";
+        CardName.Size = new System.Drawing.Size(285, 27);
+        CardName.TabIndex = 1;
+        // 
+        // EnterButton
+        // 
+        EnterButton.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        EnterButton.Location = new System.Drawing.Point(324, 15);
+        EnterButton.Name = "EnterButton";
+        EnterButton.Size = new System.Drawing.Size(146, 27);
+        EnterButton.TabIndex = 2;
+        EnterButton.Text = "ENTER";
+        EnterButton.UseVisualStyleBackColor = true;
+        EnterButton.Click += EnterButton_Click;
+        // 
+        // Rulings
+        // 
+        Rulings.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        Rulings.Location = new System.Drawing.Point(15, 48);
+        Rulings.Name = "Rulings";
+        Rulings.ReadOnly = true;
+        Rulings.Size = new System.Drawing.Size(332, 395);
+        Rulings.TabIndex = 3;
+        Rulings.Text = "";
+        // 
         // RulingsScreen
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         BackColor = System.Drawing.Color.FromArgb(((int)((byte)192)), ((int)((byte)192)), ((int)((byte)255)));
         ClientSize = new System.Drawing.Size(520, 453);
+        Controls.Add(Rulings);
+        Controls.Add(EnterButton);
+        Controls.Add(CardName);
         Controls.Add(BackButton);
         Text = "RulingsScreen";
         ResumeLayout(false);
+        PerformLayout();
     }
+
+    private System.Windows.Forms.Button EnterButton;
+    private System.Windows.Forms.RichTextBox Rulings;
+
+    private System.Windows.Forms.TextBox CardName;
 
     private System.Windows.Forms.Button BackButton;
 
--- RulingsScreen.cs
+++ RulingsScreen.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text.Json.Nodes;
+
 namespace Deck_Randomiser_2;
 
 public partial class RulingsScreen : Form
@@ -12,5 +15,51 @@ public partial class RulingsScreen : Form
         var menuScreen = new MenuScreen();
         menuScreen.Show();
         this.Close();
+    }
+    
+    private void Scryfall_Get()
+    {
+        using (var client = new WebClient())
+        {
+            client.Headers.Add(HttpRequestHeader.Accept, "image/png");
+            client.Headers.Add(HttpRequestHeader.UserAgent, "Deck_Randomiser_2");
+            string uri = "https://api.scryfall.com/cards/";
+            uri += CardName.Text;
+            uri += "/rulings";
+            try
+            {
+                client.DownloadFile(uri, "cardrulings.json");
+            }
+            catch (Exception e)
+            {
+                Rulings.Text = e.Message + "\nHTTP Error";
+            }
+        }
+    }
+
+    private void ParseCardRulings()
+    {
+        string allRulings = "";
+        if (File.Exists("cardrulings.json")) {
+            string jsonString = File.ReadAllText("cardrulings.json");
+            JsonNode obj = JsonObject.Parse(jsonString);
+            for (int i = 0; i<obj[2].AsArray().Count; i++) {
+                string ruling = obj[2][i][4].ToString();
+                allRulings += ruling + "\n\n";
+            }
+            Rulings.Text = allRulings;
+        }
+        else
+        {
+            Rulings.Text = "File 'cardrulings.json' not found";
+        }
+
+    }
+
+    private void EnterButton_Click(object sender, EventArgs e)
+    {
+        Rulings.Text = "";
+        Scryfall_Get();
+        ParseCardRulings();
     }
 }
